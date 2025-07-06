fd87da513b7358dd76c118db78a6f845589a3ed6
Luke Forehand <>
2025-07-05 23:54:38 -0500

    dry

--- main.py
+++ main.py
@@ -1,66 +1,73 @@
 #!/usr/bin/env python3
 
+import datetime
+import json
 import textwrap
 import typer
 
-from gremllm import Gremllm as Gremlin
+from gremllm import Gremllm
 from rich import print
 from rich.prompt import Prompt
+from typing import NamedTuple
 
 model_version = "anthropic/claude-3-5-sonnet-20241022"
 
 app = typer.Typer(add_completion=False)
 
 @app.command()
-def play_text_adventure(game_description: str):
-    color = "green"
-    game = Gremlin(f"text adventure with description: {game_description}", model=model_version)
-    description = game.description()
+def play_text_adventure(description: str):
+    print("loading")
+    game = gremlin(description, "green")
+    description = game.gremlin.description()
     while True:
-        cmd = Prompt.ask(prompt(f"[{color}]{description}[/{color}]"))
-        if cmd == "quit":
-            print("exiting game")
-            break
+        cmd = Prompt.ask(wrap(f"[{game.color}]{description}[/{game.color}]"))
+        check_quit(cmd, "exiting game")
         print(f"thinking...")
-        description = game.next_state(cmd)
-
+        description = game.gremlin.next_state(cmd)
 
 @app.command()
 def start_conversation(person: str):
-    color = "red"
-    person = Gremlin(person, model=model_version)
-    name = person.name
-    answer = f"[{color}]{name}[/{color}]: What is your question?"
+    print("loading")
+    person = gremlin(person, "red")
+    answer = f"[{person.color}]{person.name}[/{person.color}]: What is your question?"
     while True:
-        question = Prompt.ask(prompt(answer))
-        if question == "quit":
-            print("good bye")
-            break
-        print(f"[{color}]{name}[/{color}] is thinking...")
-        answer = person.answer_question(question)
-    
+        question = Prompt.ask(wrap(answer))
+        check_quit(question, "good bye")
+        person.print_wait()
+        answer = person.gremlin.answer_question(question)
 
 @app.command()
-def watch_conversation(person1: str, person2: str): 
-    color1 = "red"
-    color2 = "green"
-    person1 = Gremlin(person1, model=model_version)
-    person2 = Gremlin(person2, model=model_version)
-    name1 = person1.name
-    name2 = person2.name
-    print(f"waiting for [{color1}]{name1}[/{color1}] to talk...")
-    words = person1.talk()
-    print(f"{name1}: [{color1}]{textwrap.fill(str(words), width=80)}[/{color1}]")
+def watch_conversation(person1: str, person2: str):
+    print("loading")
+    p1 = gremlin(person1, "red")
+    p2 = gremlin(person2, "green")
+    p1.print_wait()
+    words = p1.gremlin.talk()
+    print(f"{p1.name}: [{p1.color}]{textwrap.fill(str(words), width=80)}[/{p1.color}]")
     while True:
-        print(f"waiting for a response from [{color2}]{name2}[/{color2}]...")
-        words = person2.respond_to(words)
-        print(f"{name2}: [{color2}]{textwrap.fill(str(words), width=80)}[/{color2}]")
-        color2, color1 = color1, color2
-        person2, person1 = person1, person2
-        name2, name1 = name1, name2
+        p2.print_wait()
+        words = p2.gremlin.respond_to(words)
+        print(f"{p2.name}: [{p2.color}]{textwrap.fill(str(words), width=80)}[/{p2.color}]")
+        p2, p1 = p1, p2
 
-def prompt(msg):
+def wrap(msg):
     return textwrap.fill(str(msg), width=80) + "\n"
+
+def gremlin(description, color="red"):
+    gremllm = Gremllm(description, model=model_version)
+    return Gremlin(gremllm, gremllm.name, color)
+
+class Gremlin(NamedTuple):
+    gremlin: Gremllm
+    name: str
+    color: str
+    def print_wait(self):
+        print(f"waiting for [{self.color}]{self.name}[/{self.color}]...")
+
+def check_quit(cmd, msg):
+    if cmd == "quit":
+        print(msg)
+        exit(0)
 
 if __name__ == "__main__":
     app()
