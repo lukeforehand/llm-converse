6f8e1fdf6b121c027d3eb3539a94602dab0b4f3f
Luke Forehand <>
2025-07-06 23:01:55 -0500

    adding scenes

--- main.py
+++ main.py
@@ -2,7 +2,9 @@
 
 import datetime
 import json
+import random
 import textwrap
+import traceback
 import typer
 
 from gremllm import Gremllm
@@ -39,23 +41,34 @@ def start_conversation(person: str):
 @app.command()
 def watch_conversation(person1: str, person2: str):
     print("loading")
-    p1 = gremlin(person1, "red")
-    p2 = gremlin(person2, "green")
+    p1 = actor(person1, "red")
+    p2 = actor(person2, "green")
     p1.print_wait()
-    words = p1.gremlin.talk()
-    print(f"{p1.name}: [{p1.color}]{textwrap.fill(str(words), width=80)}[/{p1.color}]")
-    while True:
-        p2.print_wait()
-        words = p2.gremlin.respond_to(words)
-        print(f"{p2.name}: [{p2.color}]{textwrap.fill(str(words), width=80)}[/{p2.color}]")
-        p2, p1 = p1, p2
+    scene = p1.next_scene()
+    with open(f"convo-{p1.name}-{p2.name}-{datetime.datetime.now()}.json", "w") as output:
+        write_scene(scene, output)
+        print(f"{p1.name}: [{p1.color}]{textwrap.fill(str(scene), width=80)}[/{p1.color}]")
+        while True:
+            p2.print_wait()
+            scene = p2.next_scene(scene)
+            write_scene(scene, output)
+            print(f"{p2.name}: [{p2.color}]{textwrap.fill(str(scene), width=80)}[/{p2.color}]")
+            p2, p1 = p1, p2
 
 def wrap(msg):
     return textwrap.fill(str(msg), width=80) + "\n"
 
+def write_scene(scene, output):
+    output.write(f"{scene}\n")
+    output.flush()
+
 def gremlin(description, color="red"):
     gremllm = Gremllm(description, model=model_version)
-    return Gremlin(gremllm, gremllm.name, color)
+    return Gremlin(gremllm, str(gremllm.name), color)
+
+def actor(description, color="red"):
+    gremllm = Gremllm(description, model=model_version)
+    return Actor(gremllm, str(gremllm.name), color)
 
 class Gremlin(NamedTuple):
     gremlin: Gremllm
@@ -64,10 +77,24 @@ class Gremlin(NamedTuple):
     def print_wait(self):
         print(f"waiting for [{self.color}]{self.name}[/{self.color}]...")
 
+class Actor(Gremlin):
+    def next_scene(self, previous_scene: str = None):
+        scene = self.gremlin.next_scene_actor_line_emotion_and_gesture_json(self.name, previous_scene)
+        print(scene)
+        if "Error executing code" in scene:
+            print("trying again")
+            return self.next_scene(previous_scene)
+        if isinstance(scene, dict):
+            return scene
+        return json.loads(scene)
+
 def check_quit(cmd, msg):
     if cmd == "quit":
         print(msg)
         exit(0)
 
 if __name__ == "__main__":
-    app()
+    try:
+        app()
+    except Exception:
+        traceback.print_exc()
